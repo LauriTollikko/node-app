7a9a0aec5cf3af58ecec0a78e3110ee41be5fb29
Lauri <lauri.tollikko@gmail.com>
2019-03-31 16:28:16 +0300

    added frontend app

---
+++ build/precache-manifest.5173eca5d5ca2feb1d7918dbb99d7379.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0175adc694531b8d58c4",
+    "url": "/static/js/main.0175adc6.chunk.js"
+  },
+  {
+    "revision": "9a6583ff750e7f19cae6",
+    "url": "/static/js/1.9a6583ff.chunk.js"
+  },
+  {
+    "revision": "0175adc694531b8d58c4",
+    "url": "/static/css/main.066e6819.chunk.css"
+  },
+  {
+    "revision": "8323795d95f366482735053ea0144fe5",
+    "url": "/index.html"
+  }
+];
